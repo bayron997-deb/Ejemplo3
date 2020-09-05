5285033ef98d3e359cab9dc89e358fe6ba2c1615
Bayron <b.munooz15@ufromail.cl>
2020-09-05 17:05:02 -0400

    se agregan atributos y constructor a colegio

--- src/main/java/funciones/colegio.java
+++ src/main/java/funciones/colegio.java
@@ -1,7 +1,26 @@
 package funciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * esta clase administrara los estudiantes y los profesores
  */
 public class colegio {
+    /**
+     * arraylist de profesores
+     */
+    private List<Professor> listaProfesores = new ArrayList<Professor>();
+    /**
+     * arraylist de estudiantes
+     */
+    private List<Student> listaEstudiantes = new ArrayList<Student>();
+
+    //constructores
+
+    public colegio() {
+    }
+    //metodos
+
 
 }
