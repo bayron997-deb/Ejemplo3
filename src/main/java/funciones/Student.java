10ed33e31bb2d8611733c72bc863cbe1ea014ae5
Bayron <b.munooz15@ufromail.cl>
2020-09-05 16:41:40 -0400

    agregar atributos a student

--- src/main/java/funciones/Student.java
+++ src/main/java/funciones/Student.java
@@ -4,4 +4,28 @@ package funciones;
  * tiene atributos de un estudiante
  */
 public class Student {
+    //Atributos
+
+    /**
+     * ID del estudiante
+     */
+    private String studentID;
+    /**
+     * promedio notas
+     */
+    private double averageRank;
+
+    /**
+     * ver si puede inscribirse
+     */
+    public Boolean isEligibleToRoll() {
+        return true;
+    }
+
+    /**
+     * seminarios de estudiante
+     */
+    public void setSeminarsTaken() {
+
+    }
 }
