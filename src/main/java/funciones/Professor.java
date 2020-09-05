317f09a8e855621e31bdfec30aa78ce621f27c01
Bayron <b.munooz15@ufromail.cl>
2020-09-05 16:42:36 -0400

    Agregar comentarios a clase professor

--- src/main/java/funciones/Professor.java
+++ src/main/java/funciones/Professor.java
@@ -4,5 +4,10 @@ package funciones;
  * Esta clase posee atributos para identificar a un o varios profesores
  */
 public class Professor {
+    //atributos
+    
+    /**
+     *  salario profesor
+     */
     private String salario;
 }
