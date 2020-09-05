a10bdc6dfd5b0eaa35e815257541945218115c4e
Bayron <b.munooz15@ufromail.cl>
2020-09-05 18:37:13 -0400

    agregar constructor de la ventana mas un metodo para la estructura del panel

--- src/main/java/guis/menuColegio.java
+++ src/main/java/guis/menuColegio.java
@@ -1,6 +1,7 @@
 package guis;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class menuColegio extends JFrame {
     // Atributos
@@ -32,4 +33,59 @@ public class menuColegio extends JFrame {
      * Botón para salir del programa
      */
     private JButton botonSalir;
+
+    //constructor
+    public menuColegio() {
+        //metodo para iniciar
+        iniciar();
+        //diseño
+        //titulo
+        setTitle("colegio gestor");
+        //tamaño
+        setSize(600, 600);
+        //inicar centrada
+        setLocationRelativeTo(null);
+        //cerrar ventana con la X
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+    private void iniciar() {
+        panel = new JPanel();
+        // Usa el BoxLayout (para mostrar los botones de arriba a abajo
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        // Instancia el label
+        nombrecolegio = new JLabel("colegio 2");
+        // Centra el nombre  (con los botones se hace lo mismo)
+        nombrecolegio.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // Instancia los botones
+        botonAgregarAlumno = new JButton("Nuevo estudiante");
+        botonAgregarAlumno.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        botonAgregarProfesor = new JButton("Nuevo profesor");
+        botonAgregarProfesor.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        botonlistaestudiantes = new JButton("Lista de estudiantes");
+        botonlistaestudiantes.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        botonlistaprofes = new JButton("Lista de profesores");
+        botonlistaprofes.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        botonSalir = new JButton("Salir del programa");
+        botonSalir.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        //añadir botones al panel
+        panel.add(nombrecolegio);
+        panel.add(botonAgregarAlumno);
+        panel.add(botonAgregarProfesor);
+        panel.add(botonlistaestudiantes);
+        panel.add(botonlistaprofes);
+        panel.add(botonSalir);
+        //añadir panel a la ventana
+        add(panel);
+
+
+    }
+
+
 }
