715a89a164934e3553d671b3d7f2e485e351007d
Bayron <b.munooz15@ufromail.cl>
2020-09-05 19:54:26 -0400

    se añade funcion para el boton guardar

--- src/main/java/guis/addProfessor.java
+++ src/main/java/guis/addProfessor.java
@@ -1,6 +1,9 @@
 package guis;
 
+import ejecutador.main;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class addProfessor extends addPerson {
@@ -8,7 +11,7 @@ public class addProfessor extends addPerson {
     /**
      * ingresar salario
      */
-    private  JButton jButtonsalario = new JButton("ingrese salario");
+    private JButton jButtonsalario = new JButton("ingrese salario");
     private JTextField textFielsalario = new JTextField(10);
 
     //constructor
@@ -22,8 +25,28 @@ public class addProfessor extends addPerson {
 
     public void iniciar() {
         super.iniciar();
+        //añadir botones al panel
         panel.add(jButtonsalario);
         panel.add(textFielsalario);
+        //añadir panel a la ventana
         add(panel);
+        guardar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                main.colegio.nuevoProfesor(textFieldname.getText(), textPhoneNumber.getText(), textFieldemail.getText(), textFielsalario.getText(), textFieldStreet.getText(), textFieldcity.getText(), textFieldStreet.getText(), textFielPostalCode.getText(), textFielCountry.getText());
+                //setear texto
+                textFieldname.setText("");
+                textPhoneNumber.setText("");
+                textFieldemail.setText("");
+                textFielsalario.setText("");
+                textFieldStreet.setText("");
+                textFieldcity.setText("");
+                textFieldStreet.setText("");
+                textFielPostalCode.setText("");
+                textFielCountry.setText("");
+                //cerrar
+                dispose();
+            }
+        });
     }
 }
