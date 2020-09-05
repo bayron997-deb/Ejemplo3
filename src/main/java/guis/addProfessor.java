c25ea4a4c76bc4dfde3cc4ea065d9068c12f31c5
Bayron <b.munooz15@ufromail.cl>
2020-09-05 19:08:26 -0400

    atributos de las clases addprofessor y addstudent

--- src/main/java/guis/addProfessor.java
+++ src/main/java/guis/addProfessor.java
@@ -1,4 +1,52 @@
 package guis;
 
-public class addProfessor {
+import javax.swing.*;
+
+public class addProfessor extends JFrame {
+    // Atributos
+    /**
+     * nombre del programa
+     */
+    private JLabel labelIngreseNombre;
+    /**
+     *  ingresar nombre
+     */
+    private JTextField textFieldname;
+    /**
+     *  ingresar numero
+     */
+    private JTextField textPhoneNumber;
+    /**
+     * ingresar email
+     */
+    private JTextField textFieldemail;
+    /**
+     * ingresar salario
+     */
+    private JTextField textFielsalario;
+    /**
+     *  ingresar calle
+     */
+    private JTextField textFieldStreet;
+    /**
+     *  ingresar ciudad
+     */
+    private JTextField textPhoneCiudad;
+    /**
+     * ingresar estado
+     */
+    private JTextField textFielEstado;
+    /**
+     * ingresar codigo postal
+     */
+    private JTextField textFielPostalCode;
+    /**
+     * ingresar pais
+     */
+    private JTextField textFielCountry;
+    /**
+     * boton guardar
+     */
+    private JButton guardar;
+
 }
--- src/main/java/guis/addStudent.java
+++ src/main/java/guis/addStudent.java
@@ -1,4 +1,56 @@
 package guis;
 
-public class addStudent {
+import javax.swing.*;
+
+public class addStudent extends JFrame {
+    // Atributos
+    /**
+     * nombre del programa
+     */
+    private JLabel labelIngreseNombre;
+    /**
+     *  ingresar nombre
+     */
+    private JTextField textFieldname;
+    /**
+     *  ingresar numero
+     */
+    private JTextField textPhoneNumber;
+    /**
+     * ingresar email
+     */
+    private JTextField textFieldemail;
+    /**
+     * ingresar studentID
+     */
+    private JTextField textFielstudentID;
+    /**
+     * ingresar promedio
+     */
+    private JTextField textFielaverageRank;
+    /**
+     *  ingresar calle
+     */
+    private JTextField textFieldStreet;
+    /**
+     *  ingresar ciudad
+     */
+    private JTextField textPhoneCiudad;
+    /**
+     * ingresar estado
+     */
+    private JTextField textFielEstado;
+    /**
+     * ingresar codigo postal
+     */
+    private JTextField textFielPostalCode;
+    /**
+     * ingresar pais
+     */
+    private JTextField textFielCountry;
+    /**
+     * boton guardar
+     */
+    private JButton guardar;
+
 }
