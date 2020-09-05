f5427c9c6bccba306a512e1e0de984b3d0c91061
Bayron <b.munooz15@ufromail.cl>
2020-09-05 18:25:36 -0400

    instanciar package gui en package ejecutador para ver las pruebas del diseño de la ventana principal

--- src/main/java/ejecutador/main.java
+++ src/main/java/ejecutador/main.java
@@ -1,13 +1,17 @@
 package ejecutador;
 
+import guis.menuColegio;
+
 /**
  * Clase que ejecutara el programa completo
- *@author Bayron
+ *
+ * @author Bayron
  */
 
 public class main {
     public static void main(String[] args) {
-
+        menuColegio v = new menuColegio();
+        v.setVisible();
     }
 }
 
