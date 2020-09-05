6002c1dc0c176856e1cd1e713de36f7aecf5a47f
Bayron <b.munooz15@ufromail.cl>
2020-09-05 18:22:52 -0400

    atributos de ventana principal

--- src/main/java/guis/menuColegio.java
+++ src/main/java/guis/menuColegio.java
@@ -3,4 +3,33 @@ package guis;
 import javax.swing.*;
 
 public class menuColegio extends JFrame {
+    // Atributos
+    /**
+     * Panel principal
+     */
+    private JPanel panel;
+    /**
+     * Label con el nombre del colegio
+     */
+    private JLabel nombrecolegio;
+    /**
+     * Botón para crear un profesor
+     */
+    private JButton botonAgregarProfesor;
+    /**
+     * Botón para crear un alumno
+     */
+    private JButton botonAgregarAlumno;
+    /**
+     * Botón para ver lista profesores
+     */
+    private JButton botonlistaprofes;
+    /**
+     * Botón para ver lista estudiantes
+     */
+    private JButton botonlistaestudiantes;
+    /**
+     * Botón para salir del programa
+     */
+    private JButton botonSalir;
 }
