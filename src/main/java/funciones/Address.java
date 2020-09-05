28d2bea29ea30871d73eb085ae02c3df57c25571
Bayron <b.munooz15@ufromail.cl>
2020-09-05 16:46:10 -0400

    agregar metodos y atributos a address

--- src/main/java/funciones/Address.java
+++ src/main/java/funciones/Address.java
@@ -5,4 +5,41 @@ package funciones;
  */
 
 public class Address {
+    //Atributos
+
+    /**
+     * nombre persona
+     */
+    private String street;
+    /**
+     * nombre persona
+     */
+    private String city;
+    /**
+     * nombre persona
+     */
+    private String state;
+    /**
+     * nombre persona
+     */
+    private String postalCode;
+    /**
+     * nombre persona
+     */
+    private String country;
+
+    //metodos
+
+    /**
+     * nombre persona
+     */
+    public Boolean validate(){
+        return true;
+    }
+    /**
+     * nombre persona
+     */
+    public void outPutAsLabel(){
+
+    }
 }
